2329ac575f51f1d27e0933ae6a892f3f26e08583
SURYA PRAKASH KALYANAM <suryaprakashk1805@gmail.com>
2023-03-19 23:28:52 +0530

    Prediction PipeLine

--- src/utils.py
+++ src/utils.py
@@ -32,3 +32,11 @@ def evaluate_model(X_train,Y_train,X_test,Y_test,models:dict):
 
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)        
